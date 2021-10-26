b59c386eeaebfb56ce0330edee12158189c8114e
abu-bakarr <abangura14@yahoo.com>
2021-10-26 12:13:24 +0000

    buble exer adv

---
+++ 100-shell_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * Done by Abubakarr Bangura
+ * @array: the poiter to the array
+ * @size: the size of the array
+ */
