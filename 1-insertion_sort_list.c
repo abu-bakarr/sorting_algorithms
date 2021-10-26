0724feff4668ecd737643a80690f7add0e20c108
abu-bakarr <abangura14@yahoo.com>
2021-10-26 12:04:08 +0000

    buble exm 2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,39 +1,76 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: list double point
+ * insertion_sort_list - is a simple sorting algorithm that builds
+ * the final sorted array (or list) one item at a time.
+ *
+ * @list: List of an array of integers.
+ *
+ * Return: Void Function
  */
+
 void insertion_sort_list(listint_t **list)
 {
-listint_t *next_node = NULL;
-if (list == NULL || *list == NULL || (*list)->next == NULL)
-{
-return;
-}
-next_node = (*list)->next;
-while (next_node != NULL)
-{
-while (next_node->previous && next_node->n < next_node->previous->n)
-{
-next_node->previous->next = next_node->next;
-if (next_node->next != NULL)
-{
-next_node->next->previous = next_node->previous;
-}
-next_node->next = next_node->previous;
-next_node->previous = next_node->previous->previous;
-next_node->next->previous = next_node;
-if (next_node->previous == NULL)
-{
-*list = next_node;
-}
-else
-{
-next_node->previous->next = next_node;
-}
-print_list(*list);
-}
-next_node = next_node->next;
-}
+	listint_t *tmpValue = *list, *tmpValue1 = tmpValue->next;
+	int index = 0, i;
+
+	if (*list == NULL || list == NULL)
+		return;
+	if ((*list)->next == NULL && (*list)->previous == NULL)
+		return;
+	while (tmpValue1 != NULL)
+	{
+		if (tmpValue->n > tmpValue1->n)
+		{
+			tmpValue1->previous = tmpValue->previous;
+			tmpValue->next = tmpValue1->next;
+			tmpValue->previous = tmpValue1;
+			tmpValue1->next = tmpValue;
+			if (tmpValue1->previous == NULL)
+			{
+				if (tmpValue->next != NULL)
+					tmpValue->next->previous = tmpValue;
+				*list = tmpValue1;
+			}
+			else if (tmpValue->next == NULL && tmpValue1->previous != NULL)
+				tmpValue1->previous->next = tmpValue1;
+			else if (tmpValue1->previous != NULL && tmpValue->next != NULL)
+			{
+				tmpValue->next->previous = tmpValue;
+				tmpValue1->previous->next = tmpValue1;
+			}
+			print_list(*list);
+			tmpValue = tmpValue1->previous;
+			while (tmpValue != NULL)
+			{
+				if (tmpValue->n > tmpValue1->n)
+				{
+					tmpValue1->previous = tmpValue->previous;
+					tmpValue->next = tmpValue1->next;
+					tmpValue->previous = tmpValue1;
+					tmpValue1->next = tmpValue;
+					if (tmpValue1->previous == NULL)
+					{
+						tmpValue->next->previous = tmpValue;
+						*list = tmpValue1;
+					}
+					else
+					{
+						tmpValue->next->previous = tmpValue;
+						tmpValue1->previous->next = tmpValue1;
+					}
+					print_list(*list);
+				}
+				tmpValue1 = tmpValue;
+				tmpValue = tmpValue->previous;
+			}
+		}
+		tmpValue = *list;
+		index += 1;
+		for (i = 0; index > i; i++)
+		{
+			tmpValue = tmpValue->next;
+			tmpValue1 = tmpValue->next;
+		}
+	}
 }
